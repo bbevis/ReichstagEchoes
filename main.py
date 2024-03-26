f5f4f5c318348ba40e8200b97b3ffb2ea18158bd
bbevis <burintbevis@hotmail.com>
2024-03-26 09:35:35 +0000

    BERT

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 
 import pandas as pd
-from googletrans import Translator
+import translators as ts
 import json
+from bertopic import BERTopic
+from bertopic.representation import KeyBERTInspired
 
 import os
 print(os.getcwd())
 # Load JSON data
-with open('..../Data/german_reichstag_sample_30.json', 'r') as file:
-    data = json.load(file)
+with open('german_reichstag_sample_30.json', 'r') as file:
+    df = pd.DataFrame(json.load(file))
+    
+
+print(df.shape)
+text_eng = df['text'].apply(lambda x: ts.translate_text(x, translator='google', to_language='en'))
+
+# Print translated DataFrame
+# print(df)
